e9ff6f00d8fd8e1b66dab180e734b6febebe6ce0
piotrb <piotr.blizewski@gmail.cok>
2020-03-29 20:13:55 +0200

    Wyklad 3

---
+++ Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.DAL;
+using Cw3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+	[Route("api/[controller]")]
+    
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+		
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+		
+        [HttpPut]
+        public IActionResult PutStudent(int studentId)
+        {
+            if (studentId != 0)
+            {
+                return Ok("Zaktualizowano");
+            }
+
+            return NotFound("Błąd aktualizacji");
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteStudent(int studentId)
+        {
+            if (studentId != 0)
+            {
+                return Ok("Pomyślnie usunięto");
+            }
+
+            return NotFound("Błąd usuwania");
+
+        }
+    }
+}
